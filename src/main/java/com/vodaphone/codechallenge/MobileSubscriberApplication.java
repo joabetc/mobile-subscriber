191c5e15c78b393fec76a36874c1b59dd4145dff
Joabe Costa <joabetc@hotmail.com>
2019-03-05 16:27:43 -0300

    First commit

---
+++ src/main/java/com/vodaphone/codechallenge/MobileSubscriberApplication.java
@@ -0,0 +1,13 @@
+package com.vodaphone.codechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileSubscriberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileSubscriberApplication.class, args);
+	}
+
+}
