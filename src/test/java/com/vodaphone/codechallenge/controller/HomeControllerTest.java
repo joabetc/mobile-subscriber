397e0638513a1efb2958b44dfdf709ae27380acd
Joabe Costa <joabetc@hotmail.com>
2019-03-06 20:46:11 -0300

    Creating a unit test class

---
+++ src/test/java/com/vodaphone/codechallenge/controller/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package com.vodaphone.codechallenge.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+  @Test
+  public void contextLoads() throws Exception {
+    fail("Not yet implemented");
+  }
+
+}
