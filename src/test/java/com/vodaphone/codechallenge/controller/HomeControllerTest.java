24d00cd959adc6913dbadafee277207887067e9a
Joabe Costa <joabetc@hotmail.com>
2019-03-06 20:51:45 -0300

    Failing test for context loading

--- src/test/java/com/vodaphone/codechallenge/controller/HomeControllerTest.java
+++ src/test/java/com/vodaphone/codechallenge/controller/HomeControllerTest.java
@@ -1,14 +1,23 @@
 package com.vodaphone.codechallenge.controller;
 
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HomeControllerTest {
 
+  @Autowired
+  private HomeController homeController;
+  
   @Test
   public void contextLoads() throws Exception {
-    fail("Not yet implemented");
+    assertThat(homeController).isNotNull();
   }
 
 }
