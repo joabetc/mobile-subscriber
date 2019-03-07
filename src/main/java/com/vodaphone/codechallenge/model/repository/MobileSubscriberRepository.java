6773942109cb0d5f36198e762db30795da91a12a
Joabe Costa <joabetc@hotmail.com>
2019-03-06 23:14:57 -0300

    Creating a JPARepository and it's test

---
+++ src/main/java/com/vodaphone/codechallenge/model/repository/MobileSubscriberRepository.java
@@ -0,0 +1,12 @@
+package com.vodaphone.codechallenge.model.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.vodaphone.codechallenge.model.MobileSubscriber;
+
+public interface MobileSubscriberRepository extends JpaRepository<MobileSubscriber, Integer> {
+
+  List<MobileSubscriber> findAll();
+}
---
+++ src/test/java/com/vodaphone/codechallenge/model/repository/MobileSubscriberRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.vodaphone.codechallenge.model.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.vodaphone.codechallenge.model.MobileSubscriber;
+import com.vodaphone.codechallenge.model.ServiceType;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class MobileSubscriberRepositoryTest {
+
+  @Autowired
+  private TestEntityManager entityManager;
+  
+  @Autowired
+  private MobileSubscriberRepository mobileSubscriberRepository;
+  
+  @Test
+  public void whenFindAll_thenReturnMobileSubscriberList() {
+
+    MobileSubscriber mobileSubscriber = new MobileSubscriber("35699123456", 1, 1, ServiceType.MOBILE_PREPAID);
+    entityManager.persist(mobileSubscriber);
+    entityManager.flush();
+    
+    List<MobileSubscriber> resultList = mobileSubscriberRepository.findAll();
+    
+    assertThat(resultList).isNotEmpty().contains(mobileSubscriber);
+  }
+
+}
