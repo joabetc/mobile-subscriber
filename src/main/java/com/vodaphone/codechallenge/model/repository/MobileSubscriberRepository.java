f79958fee4c9b7708e76a2cf68a78267f809a2ee
Joabe Costa <joabetc@hotmail.com>
2019-03-07 20:29:13 -0300

    Making the new tests pass

--- src/main/java/com/vodaphone/codechallenge/model/repository/MobileSubscriberRepository.java
+++ src/main/java/com/vodaphone/codechallenge/model/repository/MobileSubscriberRepository.java
@@ -2,9 +2,14 @@ package com.vodaphone.codechallenge.model.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.vodaphone.codechallenge.model.MobileSubscriber;
+import com.vodaphone.codechallenge.model.ServiceType;
 
 public interface MobileSubscriberRepository extends CrudRepository<MobileSubscriber, Integer> {
 
@@ -15,4 +20,13 @@ public interface MobileSubscriberRepository extends CrudRepository<MobileSubscri
   List<MobileSubscriber> findByCustomerIdOwner(Integer id);
 
   List<MobileSubscriber> findByCustomerIdUser(Integer id);
+
+  @Transactional
+  @Modifying
+  @Query("update MobileSubscriber mo set mo.serviceType = ?2 where mo.msisdn = ?1")
+  int setPlan(String msisdn, ServiceType serviceType);
+
+  @Transactional
+  int deleteByMsisdn(String string);
+  
 }
