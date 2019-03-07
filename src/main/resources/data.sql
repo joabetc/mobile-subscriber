a49f80dc05f758f0c2c3cc3340bf0e76f6d3fd7b
Joabe Costa <joabetc@hotmail.com>
2019-03-06 22:14:01 -0300

    Creating data file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into MOBILE_SUBSCRIBER (id, msisdn, customer_id_owner, customer_id_user, service_type, service_start_date) values (1, '35699123456', 1, 1, 'MOBILE_PREPAID', DATEDIFF('SECOND', DATE '1970-01-01', CURRENT_TIMESTAMP()) * 1000)
