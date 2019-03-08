4cb652537787b7034620de3a9b3833941eaa68c9
Joabe Costa <joabetc@hotmail.com>
2019-03-07 23:13:31 -0300

    Adding method to get opposite service type

--- src/main/java/com/vodaphone/codechallenge/model/ServiceType.java
+++ src/main/java/com/vodaphone/codechallenge/model/ServiceType.java
@@ -2,5 +2,14 @@ package com.vodaphone.codechallenge.model;
 
 public enum ServiceType {
 
-  MOBILE_PREPAID, MOBILE_POSTPAID
+  MOBILE_PREPAID, MOBILE_POSTPAID;
+  
+  public static ServiceType getOpposite(ServiceType serviceType) {
+    for (ServiceType s : values()) {
+      if (!s.equals(serviceType)) {
+        return s;
+      }
+    }
+    return serviceType;
+  }
 }
