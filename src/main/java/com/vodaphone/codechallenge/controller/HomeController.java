5d916cb54192c73ca6dcfe10fcc6714f87bc4dfe
Joabe Costa <joabetc@hotmail.com>
2019-03-08 00:05:21 -0300

    Making the test pass

--- src/main/java/com/vodaphone/codechallenge/controller/HomeController.java
+++ src/main/java/com/vodaphone/codechallenge/controller/HomeController.java
@@ -1,8 +1,24 @@
 package com.vodaphone.codechallenge.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vodaphone.codechallenge.model.MobileSubscriber;
+import com.vodaphone.codechallenge.service.MobileSubscriberService;
+
+@RestController
+@RequestMapping("/api")
 public class HomeController {
 
+  @Autowired
+  private MobileSubscriberService mobileSubscriberService;
+  
+  @GetMapping("/subscribers")
+  public List<MobileSubscriber> getAll() {
+    return mobileSubscriberService.getAllMobileSubscribers();
+  }
 }
