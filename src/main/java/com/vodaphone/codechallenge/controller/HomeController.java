b43e5d5a107293d84598f0c38155370192a5e418
Joabe Costa <joabetc@hotmail.com>
2019-03-09 12:04:36 -0300

    Implementing some GET requests

--- src/main/java/com/vodaphone/codechallenge/controller/HomeController.java
+++ src/main/java/com/vodaphone/codechallenge/controller/HomeController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,20 @@ public class HomeController {
   @GetMapping("/subscribers")
   public List<MobileSubscriber> getAll() {
     return mobileSubscriberService.getAllMobileSubscribers();
+  }
+  
+  @GetMapping("/subscribers/{msisdn}")
+  public MobileSubscriber getByNumber(@PathVariable String msisdn) {
+    return mobileSubscriberService.getMobileSubscriberByNumber(msisdn);
+  }
+
+  @GetMapping("/subscribers/owner/{customerIdOwner}")
+  public List<MobileSubscriber> getByCustomerIdOwner(@PathVariable Integer customerIdOwner) {
+    return mobileSubscriberService.getMobileSubscriberByCustomerIdOwner(customerIdOwner);
+  }
+  
+  @GetMapping("/subscribers/user/{customerIdUser}")
+  public List<MobileSubscriber> getByCustomerIdUser(@PathVariable Integer customerIdUser) {
+    return mobileSubscriberService.getMobileSubscriberByCustomerIdUser(customerIdUser);
   }
 }
