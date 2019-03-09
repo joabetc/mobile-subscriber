327cbd73abac1b7b9ea685f6deec97e249690b97
Joabe Costa <joabetc@hotmail.com>
2019-03-09 18:45:19 -0300

    Implementing controller methods

--- src/main/java/com/vodaphone/codechallenge/controller/HomeController.java
+++ src/main/java/com/vodaphone/codechallenge/controller/HomeController.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -43,5 +48,36 @@ public class HomeController {
   @GetMapping("/subscribers/user/{customerIdUser}")
   public List<MobileSubscriber> getByCustomerIdUser(@PathVariable Integer customerIdUser) {
     return mobileSubscriberService.getMobileSubscriberByCustomerIdUser(customerIdUser);
+  }
+  
+  @PutMapping("/subscribers/{msisdn}/plan")
+  public MobileSubscriber changePlan(@PathVariable String msisdn) {
+    try {
+      return mobileSubscriberService.changeMobileSubscriberPlan(msisdn);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.NOT_FOUND, e.getMessage(), e);
+    }
+  }
+  
+  @DeleteMapping("/subscribers/{msisdn}")
+  public void deleteNumber(@PathVariable String msisdn) {
+    try {
+      mobileSubscriberService.deleteByNumber(msisdn);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.NOT_FOUND, e.getMessage(), e);
+    }
+  }
+  
+  @PostMapping("/subscribers")
+  public ResponseEntity<MobileSubscriber> createMobileSubscriber(@RequestBody MobileSubscriber mobileSubscriber) {
+    try {
+      MobileSubscriber mobileSubscriberSaved = mobileSubscriberService.createMobileSubscriber(mobileSubscriber);
+      return new ResponseEntity<MobileSubscriber>(mobileSubscriberSaved, HttpStatus.CREATED);
+    } catch (Exception e) {
+      throw new ResponseStatusException(
+          HttpStatus.CONFLICT, e.getMessage(), e);
+    }
   }
 }
