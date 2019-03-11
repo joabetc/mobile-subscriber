b292031e0b6ff45d58e59dc4b15ca1440478adde
Joabe Costa <joabetc@hotmail.com>
2019-03-11 00:56:08 -0300

    Removing data file

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-select * from MOBILE_SUBSCRIBER
---insert into MOBILE_SUBSCRIBER (id, msisdn, customer_id_owner, customer_id_user, service_type, service_start_date) values (1, '35699123456', 1, 1, 'MOBILE_PREPAID', DATEDIFF('SECOND', DATE '1970-01-01', CURRENT_TIMESTAMP()) * 1000)
